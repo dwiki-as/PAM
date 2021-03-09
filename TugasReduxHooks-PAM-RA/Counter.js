ae965301ff283e7a0c95c4f08476e99a31c8b028
dwiki-as <80322355+dwiki-as@users.noreply.github.com>
2021-03-09 23:54:17 +0700

    tugas pam-ra

---
+++ TugasReduxHooks-PAM-RA/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { tambahCounter, kurangCounter} from './redux/action'
+
+const Counter = () => {
+    const dispatch = useDispatch();
+    const counter = useSelector(data => data.counter);
+    
+    return (
+        
+        <View style={{flex:1,alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
+            <TouchableOpacity style={{backgroundColor:'#adadad',paddingBottom:5,paddingTop:5,
+            paddingLeft:20,paddingRight:20, borderRadius:60}} onPress={()=>dispatch(kurangCounter(counter))}>
+                <Text style={{fontSize:30}}>-</Text>
+            </TouchableOpacity>
+            <Text style={{fontSize:40,marginLeft:20,marginRight:20}}>{counter}</Text>
+            <TouchableOpacity style={{backgroundColor:'#adadad',paddingBottom:5,paddingTop:5,
+            paddingLeft:20,paddingRight:20, borderRadius:60}} onPress={()=>dispatch(tambahCounter(counter))}>
+                <Text style={{fontSize:30}}>+</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Counter
